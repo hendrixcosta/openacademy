63c84e0bbcab5ff319beea437f802d173b44208b
hendrixcosta <hendrixcosta@gmail.com>
2016-03-30 12:15:25 -0300

    commit inicial

---
+++ openacademy/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "OpenAcademy",
+    "summary": "Modulo da openAcademy",
+    "version": "8.0.1.0.0",
+    "category": "custom",
+    "website": "https://odoo-community.org/",
+    "author": "Hendrix Kmee",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "base",
+    ],
+
+}
---
+++ openacademy/models/course.py
@@ -0,0 +1,15 @@
+from openerp import models, fields, api
+
+
+class Course(models.Model):
+
+    _name = "course.course"
+    _description = "Course"
+
+    name = fields.Char(
+        string="Name"
+    )
+    description = fields.Text(
+        string="Descricao"
+    )
+
